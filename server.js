4655f2cb214c4c987df555b5a3d945f96369a441
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-05-09 14:15:48 +0100

    création schema et model

--- server.js
+++ server.js
@@ -4,12 +4,22 @@ const express = require('express');
 const app = express();
 app.use(express.json());//To convert=parse incoming JSON data from HTTP requests, to Json Objects easier to read for the server
 
-
 const port = 3005 //app.listen(port, ...) sets up the server to listen on that port. 
 
 const db = require('./connect-db')
 
 require('dotenv').config(); //To use the environment variable
+
+const { postAllArticles } = require('./model-doc')//on destructure les differents models
+
+const cors = require('cors')
+
+app.use(cors({
+  origin: ["http://localhost:3000"],//to access the front-end side through this URL
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}))
+
 
 //database connection: http://localhost:3005/ pour voir le message
 app.get('/', (req, res) => {
