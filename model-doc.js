5c75a120610e3ce90074e48fe34c74f60d7b3098
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-05-09 13:59:50 +0100

    all back-end setup

---
+++ model-doc.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+//création d'un schema mongoose = structure "document", dans "collection" de la database "phenixArticles" dans MongoDB
+const allArticles = mongoose.Schema(
+  {
+    type: "string",
+    imageUrl: "string",
+    infoProduit: "string",
+    auteur: "string",
+    prix: "string",
+    etat: "string",
+    code: "string"
+  },
+
+  {
+    timestamps: true,
+  }
+);
+//créeation d'un model à partir du schéma crée précedemment avec mongoose.model("collectionName", schema)
+//On stocke ce modèle dans la variable postAllArticles pour être utilisé plus facilement
+const postAllArticles = mongoose.model('allArticles', allArticles)
+
+module.exports = {postAllArticles}; //Exportation et Destructuration de la variable pour pouvoir utilisé sa valeur en écrivant son nom
