15fc8674004fb88116c5b1c69254368f07784d1f
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-12-23 14:10:58 +0100

    simplification sitemap

--- SEO/sitemap.js
+++ SEO/sitemap.js
@@ -15,38 +15,28 @@ router.get("/sitemap.xml", async (req, res) => {
     // Configuration des en-têtes pour indiquer qu'il s'agit d'un fichier XML
     res.header("Content-Type", "application/xml");
 
-    // On store notre modèle avec les properties dans la variable articles // .find({}): Récupère tous les articles dans la base de données "code": Mais que le champ "code" pour éviter trop de données
-    // Retourne des objets JavaScript simples pour optimiser les performances pour la lecture
-    const articles = await postAllArticles.find({}, "auteur _id").lean(); //pas de , entre auteur et _id car ce n'est pas une array, mais une chaine de carctère avec une liste de champs séparés par des espaces
-
-    // Construction des liens: Crée un tableau contenant les URLs pour le sitemap et priority = importances des pages pour guider le robot google
-
-    // Liens pour les pages dynamiques: On dit dynamique car la fin de leur url change en fonction du nom de l'auteur ou de l'_id de l'article
-    const dynamicLinks = articles.flatMap((article) => [
-      {
-        url: `/pageArtist/${encodeURIComponent(article.auteur)}`, // Encode l'auteur pour éviter les caractères spéciaux
-        changefreq: "weekly", // La page change fréquemment
-        priority: 1.0, // Priorité relative
-      },
-      {
-        url: `/ficheArticle/${article._id}`, //Utilisation de l'url /ficheArticle et non la route /article et de l'_id donné par mongoDB 
-        changefreq: "yearly", // La page change rarement
-        priority: 0.1, // Priorité relative
-      },
-    ]);
+    // Construction des liens: Crée un tableau contenant les URLs pour le sitemap et priority = importances des pages pour guider le robot google / Ne mettre que les pages qu'on veut voir dans les recherches google dans le sitemap
 
     // Liens pour les pages statiques: on dit statiques que leur url ne changent pas, même si pour certaines pages comme les pages accueil, artistes ou tableaux, leurs contenus changent régulièrement
     const staticLinks = [
       { url: "/", changefreq: "daily", priority: 1.0 },
-      { url: "/allArtists", changefreq: "monthly", priority: 0.8 },
-      { url: "/tableaux", changefreq: "daily", priority: 0.8 },
       { url: "/bestDeals", changefreq: "monthly", priority: 0.8 },
-      { url: "/vendre", changefreq: "yearly", priority: 0.1 },
       { url: "/concept", changefreq: "yearly", priority: 0.5 },
     ];
 
+    // J'en ai besoin pour extraire l'auteur de chaque tableau et pouvoir le mettre dans l'url dynamique de pageArtist
+    // On store notre modèle avec les properties dans la variable articles // .find({}): Récupère tous les articles dans la base de données: Mais que le champs "auteur" pour éviter trop de données
+    const articles = await postAllArticles.find({}, "auteur").lean();
+
+    // Liens pour les pages dynamiques: On dit dynamique car la fin de leur url change en fonction du nom de l'auteur de l'article
+    const dynamicLinks = articles.map((article) => ({
+      url: `/pageArtist/${encodeURIComponent(article.auteur)}`, // Encode l'auteur pour éviter les caractères spéciaux
+      changefreq: "weekly", // La page change fréquemment
+      priority: 1.0, // Priorité relative
+    }));
+
     // Combiner les liens dynamiques et statiques
-    const allLinks = [...dynamicLinks, ...staticLinks];
+    const allLinks = [...staticLinks, ...dynamicLinks];
 
     // Création du sitemap avec SitemapStream
     const stream = new SitemapStream({
